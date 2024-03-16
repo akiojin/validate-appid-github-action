3b08f0e57609ab7fb77eb27667f33f7900199851
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2024-03-16 13:49:44 +0900

    feat: ビルドターゲットを指定できるように修正

--- dist/index.js
+++ dist/index.js
@@ -24731,15 +24731,35 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(2186));
-function RemoveInvalidCharsForAppID(input) {
+function RemoveInvalidCharsForAndroid(input) {
     return input
         .toLowerCase()
         .replace(/[^a-z0-9.]/g, '') // 英小文字と数字とピリオド以外を削除
         .replace(/^[0-9]+/, '') // 先頭の数字を削除
         .replace(/\.{2,}/g, '.'); // 連続するピリオドを一つに置換
 }
+function RemoveInvalidCharsForiOS(input) {
+    return input
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '') // 英小文字と数字以外を削除
+        .replace(/^[0-9]+/, ''); // 先頭の数字を削除
+}
+function RemoveInvalidChars() {
+    const buildTarget = core.getInput('build-target').toLowerCase();
+    const domain = core.getInput('domain');
+    const appName = core.getInput('app-name');
+    if (buildTarget === 'ios') {
+        return RemoveInvalidCharsForiOS(`${domain}.${appName}`);
+    }
+    else if (buildTarget === 'android') {
+        return RemoveInvalidCharsForAndroid(`${domain}.${appName}`);
+    }
+    else {
+        throw new Error('Not supported build target. Please set ios or android.');
+    }
+}
 try {
-    const replaced = RemoveInvalidCharsForAppID(`${core.getInput('domain')}.${core.getInput('app-name')}`);
+    const replaced = RemoveInvalidChars();
     core.setOutput('app-id', replaced);
     core.exportVariable('APP_ID', replaced);
 }
